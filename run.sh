d6294516bf983a7d46c36567bab90138ce2e2a99
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-12-03 22:06:01 -0600

    codegen won't error on unused variables

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 PROG_NAME=$1
 MODE=$2
 
-reset && ./compile $MODE && ./boa example/${PROG_NAME}.boa && cat example/*.c && gcc -std=iso9899:1990 -Os -Wall -Werror -Wpedantic -Wextra example/${PROG_NAME}.c && ./a.out
+reset && ./compile $MODE && ./boa example/${PROG_NAME}.boa && cat example/*.c && gcc -std=iso9899:1990 -Os -Wall -Werror -Wpedantic -Wno-error=unused-variable -Wextra example/${PROG_NAME}.c && ./a.out
