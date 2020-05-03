ec75aa394d60134eb79ef861fc725d4bdc772bfa
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-05-03 14:28:52 -0500

    moved comment to appropriate file

--- compiler/hindley-milner.c
+++ compiler/hindley-milner.c
@@ -196,6 +196,25 @@ static void print_sets(
     }
 }
 
+/*
+ * 1) Traverse AST, and give each expression a type, and populate these types
+ *    in the TypeEquiv table.
+ *    When a concrete type is found for a given temporary type (Tx),
+ *    add it to the concrete types table.
+ *
+ * 2) Loop through TypeEquiv. For each type in the key's set, consolodate the
+ *    sets from those types into the parent type, then delete their keys from
+ *    the table TypeEquiv.
+ *    When a concrete type is found, populate ConcreteTypes with this info
+ *    tbl(key) = type
+ *
+ * 3) Iterate through AST again, and for each expression, replace its
+ *    temporary type with a concrete type. If there does not exist an entry
+ *    in the ConcreteTypes table for this type, then typechecking failed,
+ *    and the user needs to fix their types. Report a diagnostic message
+ *    informing which line and which identifier does not have a correct type.
+ */
+
 int hindley_milner(ASTNode *ast) {
     struct State state;
 
